dea500856c5e5be02c478ccadf26d1014485f8b0
Jean-Philippe Evrard <open-source@a.spamming.party>
2022-07-19 15:03:10 +0200

    Initial commit
    Add the galaxy scafolding for the collection

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ansible Collection - evrardjp.ha
+
+Documentation for the collection.
---
+++ update-upstream.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+git subtree pull --prefix roles/keepalived https://github.com/evrardjp/ansible-keepalived.git master --squash
